8aef40175068350438012b79e7f620687db1af7a
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2021-04-14 18:17:01 +0530

    Move to different folder

--- express-gql/books.js
+++ express-gql/books.js
@@ -5,12 +5,12 @@ const app = express();
 app.use(express.json());
 
 app.use('/books/:id', (req, res) => {
-  const { id } = req.params
-  console.log(`GET /books/${id}`)
+  const { id } = req.params;
+  console.log(`GET /books/${id}`);
   res.json({
     1: ['Book 1', 'Book 2', 'Book 3'],
     2: ['Book 3', 'Book 4', 'Book 5'],
-  }[id] || [])
-})
+  }[id] || []);
+});
 
 app.listen(3001, () => console.log('Listening: http://localhost:3001/books/:id'));
--- express-gql/paintings.js
+++ express-gql/paintings.js
@@ -5,12 +5,12 @@ const app = express();
 app.use(express.json());
 
 app.use('/paintings/:id', (req, res) => {
-  const { id } = req.params
-  console.log(`GET /paintings/${id}`)
+  const { id } = req.params;
+  console.log(`GET /paintings/${id}`);
   res.json({
     1: ['Painting 1', 'Painting 2', 'Painting 3'],
     2: ['Painting 3', 'Painting 4', 'Painting 5'],
-  }[req.params.id] || [])
-})
+  }[req.params.id] || []);
+});
 
 app.listen(3002, () => console.log('Listening: http://localhost:3002/paintings/:id'));
